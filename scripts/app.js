41a053b64dcb6926a1616d59ec082b0b246c65e5
Timothy Millward <silhouettetechnician@gmail.com>
2019-02-22 11:14:21 +0000

    styling mostly complete

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,6 @@
 $(() => {
 
-  const containerWidth = 1000
+  // *******************************************GLOBALS*********************************************
   const width = 30
   const height = 20
   const rightKey = 39
@@ -8,34 +8,55 @@ $(() => {
   const spaceBar = 32
   const baddieArray = []
   let playerIndex = 544
-
+  let alienPoints = 0
+  const $score = $('.score-ctn')
+  const $lives = $('.lives')
+  const $startScreen = $('#startScreen')
   const $gameBoard = $('.game-container')
+  const $playbutton = $('.playGame')
+  const $button = $('#button')
   const cellArray = []
   let direction = 1
   let movementId = null
   var shootAudio = new Audio('Sounds/shoot.wav')
   const invaderExplosion = new Audio('Sounds/invaderkilled.wav')
+
+  // *****************************************GAME INITIALISER************************************
+  $($button).on('click', () => {
+    if ($startScreen.hasClass('hidden'))
+      $startScreen.css({
+        display: 'none'
+      })
+    init()
+  })
+
   function init(){
 
     const spawnGood = () => {
+
+      // *****************************************SET PLAYER POSITION*************************************
 
       const $startingPoint = cellArray[544].addClass('goodGuy')
 
     }
     startsGrid()
     spawnGood()
+    moveBaddies()
+    keyPress()
+    scoreLives()
 
   }
-
+  // *****************************************SET UP PLAYING AREA*************************************
 
   function startsGrid(){
-
+    let looopCount = 0
     for(let i = 0; i < Math.floor(height * width); i++) {
+      looopCount ++
       const $div = $('<div></div>').addClass('cell')
       $div.attr( 'id', i)
       cellArray.push($div)
       $gameBoard.append($div)
-
+      // *****************************************SPAWN BADDIES*************************************
 
       if(i > 6 && i < 25 && i%2) {
         baddieArray.push(new Baddie(i, 20))
@@ -47,12 +68,10 @@ $(() => {
         baddieArray.push(new Baddie(i, 20))
       }
     }
+    console.log(looopCount)
   }
 
-
-
-
-
+  // *****************************************MOVE ENEMIES*************************************
   baddieArray.forEach(baddie => baddie.render())
 
   function moveBaddies() {
@@ -66,27 +85,29 @@ $(() => {
       if(direction === 66) direction = 1
       if(baddieArray.find(baddie => baddie.currentIndex % 66 === 33) && direction === -1) {
         direction = 66
-
-
       }
-      // console.log(direction)
-
-
-      // if (baddieArray.find(baddie => baddie.currentIndex >= 495) && baddieArray.find(baddie => baddie.currentIndex =< 527){
-      //   clearInterval(movementId)
-      //   console.log('you lost')
-      // }
-
-      // if
-
-
-
-
-    }  , 2000)
+    }
+    , 300)
   }
-  // if baddieArray.forEach(baddie => baddie.baddieMove())
 
-  let bullet
+  // function helloFred() {
+  //   shootingId = setInterval(() => {
+  //
+  //     // cellArray[.currentIndex]
+  //     // const random = (Math.floor(Math.random()*currentIndex.length))
+  //     // console.log(cellArray[this.currentIndex])
+  //
+  //     // const randomBullet = cellArray
+  //     // cellArray[random]
+  //     // console.log(cellArray[random])
+  //     // randomBullet.addClass('enemyBullet')
+  //   }, 2000)
+  //
+  // }
+
+
+
+  // *****************************************BULLET CLASS*************************************
 
   class Bullet {
     constructor(direction, index){
@@ -112,11 +133,16 @@ $(() => {
         cellArray[this.index].removeClass('badGuy')
         const hitAlien = baddieArray.find(baddie => baddie.currentIndex  ===  this.index)
         hitAlien.isHit = true
+        alienPoints += 20
+        $score.text(alienPoints)
+        console.log(alienPoints)
         invaderExplosion.play('Sounds/invaderkilled.wav')
         this.remove()
         clearInterval(this.bulletTimer)
       }
     }
+
+
 
     moveBullet() {
       this.bulletTimer = setInterval(() => {
@@ -129,6 +155,7 @@ $(() => {
         }
       }, 100)
     }
+
   }
 
   $(window).on('keydown', function(e){
@@ -138,49 +165,21 @@ $(() => {
     }
   })
 
-  // this.render()
-
-  //
-  //   }
-  //
-  // }
-  // $(window).on('keydown', function(e){
-  // const bullet = {
-  //   position: []
-  // }
-
-
-
-  // const bulletIndex = playerIndex
-  // if (e.keyCode === spaceBar) {
-  // $gameBoard.find('cell').eq(playerIndex).addClass('.bullet')
-
-
-  // (cellArray)[421].addClass('bullet')
-  // cellArray[bulletIndex].classList.add('bullet')
-  // bullet.position.push(bulletIndex)
-  // shootAudio.play('Sounds/shoot.wav')
-
-  //   }
-  // )
-
-
+  // *****************************************BADDIE CLASS*************************************
 
   class Baddie {
     constructor(startingIndex, points){
-      // const startingIndex = alienSpawn()
-
 
       this.startingIndex = startingIndex
       this.currentIndex = startingIndex
+
       this.points = points
       this.movesMade = 0
       this.isMovingRight = true
+      this.outOfBounds = startingIndex + 500
       this.isHit = false
       this.render()
-
     }
-
 
     render() {
       if(!this.isHit) {
@@ -188,12 +187,50 @@ $(() => {
       }
     }
 
+    checkLose() {
+      console.log('you lost')
+    }
+
+
     baddieMove() {
       cellArray[this.currentIndex].removeClass('badGuy')
       this.currentIndex += direction
-      this.render()
+      if(this.currentIndex > 500) {
+        this.checkLose()
+      } else {
+        this.movesMade ++
+        this.render()
+      }
     }
   }
+
+
+
+  //
+  // if (cellArray[this.currentIndex] >= this.outOfBounds){
+  //
+  // }
+
+
+  //     baddieShoot(){
+  // // (Math.floor(Math.random()*baddieArray.length))
+  //       // const shot = cellArray[this.currentIndex].hasClass('badGuy')
+  //       shootingId = setInterval(() => {
+  //         console.log(shot)
+  //         cellArray.currentIndex[random]
+  //         if (cellArray.hasClass('badGuy'))
+  //           console.log(cellArray.currentIndex[random])
+  //         baddieArray[this.currentIndex].removeClass('enemyBullet')
+  //         console.log(baddieArray[this.currentIndex])
+  //         this.startIndex += 33
+  //         if (this.currentIndex < 0) {
+  //           clearInterval(this.shootingId)
+  //         } else {
+  //           cellArray[this.currentIndex].addClass('enemyBullet')
+  //         }
+  //       }, 1000)
+  //
+  //     }
 
 
   // ***************************KEYPRESS FUNCTIONS*************************
@@ -202,7 +239,6 @@ $(() => {
   function keyPress(){
 
     $(window).on('keydown', function(e){
-      console.log(e.keyCode, rightKey, playerIndex)
       if (e.keyCode === leftKey && playerIndex > 528) {
         $(cellArray).eq(playerIndex)[0].removeClass('goodGuy')
         playerIndex--
@@ -218,33 +254,14 @@ $(() => {
   }
 
 
-
-  //   //     if (e.keyCode === $spaceBar) {
-  //   //       console.log('firing')
-  //   //       $gameBoard.find('.cell').eq(playerIndex).addClass('.bullet')
-  //   //
-  //   //
-  //   //       bullet.push(bullets)
-  //   //
-  // function animate() {
-  //   requestAnimationFrame(animate)
-  //   keyPress()
-  //   moveBaddies()
-  // }
-  // animate()
+  // init()
 
 
-  init()
-  keyPress()
-  moveBaddies()
+
 
   // startBtn.click(() => {
   //   startsGrid()
   //   startGame
   // })
-
-
-
-
 
 })
